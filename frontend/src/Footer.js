f4ced1307451b6e1ff8ea91ab687a534e7642579
Mihir Athale <athale.m@northeastern.com>
2024-08-09 16:32:30 -0400

    building frontend

---
+++ frontend/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+	  <footer className="bg-gray-200 p-4 text-center">
+	    <p>&copy; 2024 My React Project</p>
+	  </footer>
+);
+
+export default Footer;
+
