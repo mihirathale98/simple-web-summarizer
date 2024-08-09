4badf396765c684ef23ff7d7b05646882be3917f
Mihir Athale <athale.m@northeastern.com>
2024-08-09 15:50:47 -0400

    added models

---
+++ src/models.py
@@ -0,0 +1,41 @@
+from together import Together
+from openai import OpenAI
+from anthropic import Anthropic
+
+
+class TogetherModel:
+    def __init__(self):
+        self.together_client = Together()
+
+
+    def generate(self, prompt, model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"):
+        completion = client.chat.completions.create(
+                model=model,
+                messages=[{"role": "user", "content": prompt}],
+                )
+        return completion
+
+
+class OpenAIModel:
+    def __init__(self):
+        self.openai_client = OpenAI()
+
+
+    def generate(self, prompt, model="gpt-3.5-turbo"):
+        completion = client.chat.completions.create(
+                model=model,
+                messages=[{"role": "user", "content": prompt}],
+                )
+        return completion.choices[0].message.content
+
+
+class AnthropicModel:
+    def __init__(self):
+        self.anthropic_client = Anthropic()
+
+    def generate(self, prompt, model="claude-v1.3-100k"):
+        completion = client.completion(
+                prompt=prompt,
+                model=model,
+                )
+        return completion
