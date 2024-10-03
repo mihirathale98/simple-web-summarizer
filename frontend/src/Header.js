f9af2f26d27ff96296e97a76ab35f0ff7ee6b17c
mihirathale98 <athale.m@northeastern.edu>
2024-10-03 19:02:34 -0400

    search api

--- frontend/src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-	  <header className="bg-blue-500 p-4 text-white">
-	    <h1 className="text-2xl font-bold">My Simple React Web Page</h1>
-	  </header>
-);
-
-export default Header;
--- src/web_search_api.py
+++ src/web_search_api.py
@@ -1,20 +1,32 @@
-## Bing Search API
-
-import os
 import requests
-import json
+import os
 
-class BingSearch:
-    def __init__(self):
-        self.subscription_key = os.environ.get("BING_SEARCH_API_KEY")
-        self.url = "https://api.bing.microsoft.com/v7.0/search"
-        self.headers = {"Ocp-Apim-Subscription-Key": self.subscription_key}
+def parse_brave_search_results(search_results):
+    if search_results is None:
+        return None
+    results = search_results.get("web", {}).get("results", [])
+    if len(results) == 0:
+        return None
+    print("Found {} results".format(len(results)))
+    return results
 
-    def search(self, search_term):
-        params = {"q": search_term}
-        response = requests.get(self.url, headers=self.headers, params=params)
-        response.raise_for_status()
-        search_results = response.json()
-        return search_results
-
+def brave_search(query, count=10):
+    base_url = "https://api.search.brave.com/res/v1/web/search"
     
+    headers = {
+        "Accept": "application/json",
+        "X-Subscription-Token": os.getenv('BRAVE_API_KEY')
+    }
+    
+    params = {
+        "q": query,
+        "count": count,
+    }
+    
+    try:
+        response = requests.get(base_url, headers=headers, params=params)
+        response.raise_for_status()
+        return parse_brave_search_results(response.json()) if response.status_code == 200 else response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
