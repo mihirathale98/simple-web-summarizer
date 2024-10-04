fd5388166150d47f2b16405cf539ac347ba257f4
mihirathale98 <athale.m@northeastern.edu>
2024-10-03 20:02:58 -0400

    updated api

--- main.py
+++ main.py
@@ -2,13 +2,24 @@ from fastapi import FastAPI
 import uvicorn
 
 from src.models import TogetherModel
-from src.web_search_api import BingSearch
+from src.web_search_api import brave_search
+from src.query_gen import generate_queries
 
-
-bing_search = BingSearch()
 model = TogetherModel()
 
 
+app = FastAPI()
+
+@app.post('/search')
+def search(request: dict):
+    query = request["query"]
+    queries = generate_queries(query)
+
+    # results = {query: brave_search(query) for query in queries}
+    return {"queries": queries}
+
+
 if __name__ == "__main__":
-    #print(bing_search.search(search_term="Python"))
-    print(model.generate("Hi"))
+    uvicorn.run(app, host="0.0.0.0", port=8000)  
+
+
